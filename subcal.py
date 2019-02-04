adae5574d27264cd43883358dcd85d5132590b9f
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-04 05:02:12 -0800

    try

--- subcal.py
+++ subcal.py
@@ -1,1 +1,15 @@
-print("subnet calculator")
+class ipaddress():
+
+      #ipaddress add
+      def __init__(self,ip=[0,0,0,0],nm=[0,0,0,0]):
+          self.ip = ip
+          self.nm = nm
+      def __str__(self):
+          ipformat = ""
+          for ips in self.ip:
+              ipformat = ipformat + str(ips) +  "."
+          return("The address is:" + ipformat)
+myip = ipaddress([192,168,1,1],[255,255,255,0])
+
+print(myip)
+
